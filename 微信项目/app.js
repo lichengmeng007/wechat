3217da1ac1a3cadc5e4c7850c79e5c8f49173a4b
lichengmeng007 <44698808+lichengmeng007@users.noreply.github.com>
2018-11-18 10:34:54 +0800

    接收用户发送消息和简单的响应

--- 微信项目/app.js
+++ 微信项目/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
 const sha1 = require('sha1');
+
+const {getUserDataAsync,parseXMLDataAsync,formatMessage} = require('./utils/tools');
 const  app = express();
 
 
@@ -8,6 +10,7 @@ const  app = express();
 /*
  搭建开发者服务器
  */
+//配置对象
 const config = {
     appID:'wx9b805dbf44b34b82',
     appsecret:'e20fc9f751d195f17bfb8266892bc90a',
@@ -15,7 +18,7 @@ const config = {
 };
 
 
-app.use((req,res,next) =>{
+app.use( async(req,res,next) =>{
     console.log(req.query);
     //获取请求参数
     const{signature,echostr,timestamp,nonce} = req.query;
@@ -23,12 +26,68 @@ app.use((req,res,next) =>{
 //将排序后的参数拼接在一起，并且进行加密
     const str = sha1([timestamp,nonce,token].sort().join(''));
 
-    if(signature === str){
-        res.end(echostr);
+    /*
+     1.get 验证服务器有效性逻辑
+     2.post转发用户消息
+     */
+    if(req.method ==='GET'){
+        //验证服务器有效性
+        if(signature === str){
+            //说明来自微信服务器
+            res.end(echostr);
+
+        }else {
+            res.end('error');
+        }
+
+    }else if(req.method ==='POST'){
+        //转发用户消息并验证消息来自微信服务器
+        if(signature !== str){
+            res.end('error');
+            return;
+        }
+
+        //用户发送的消息在请求体
+        const xmlData = await getUserDataAsync(req);
+        console.log(xmlData);
+        //将用户发送过来的xml数据解析为js对象
+        const jsData =  await parseXMLDataAsync(xmlData);
+        console.log(jsData);
+
+        //格式化数据
+        const message = formatMessage(jsData);
+
+        console.log(message);
+
+
+        //初始化一个消息文本
+        let content = '你在说什么，我听不懂~';
+
+        //判断用户发送消息的内容，根据内容返回特定的响应
+        if (message.Content === '1') {  //全匹配
+            content = '好好学习，天天向上';
+        } else if (message.Content === '2') {
+            content = '生活不止眼前的苟且还有诗和远方';
+        } else if (message.Content.includes('山')) {  //半匹配
+            content = '人说山西好风光~';
+        }
+
+        //返回xml消息给微信服务器
+        let replyMessage = `<xml>
+      <ToUserName><![CDATA[${message.FromUserName}]]></ToUserName>
+      <FromUserName><![CDATA[${message.ToUserName}]]></FromUserName>
+      <CreateTime>${Date.now()}</CreateTime>
+      <MsgType><![CDATA[text]]></MsgType>
+      <Content><![CDATA[${content}]]></Content>
+      </xml>`;
+       // 微信服务器当没有接收到开发者服务器响应时，默认会请求3次开发者服务器，就会导致接口被调用多次
+       res.send(replyMessage);
+
 
     }else {
         res.end('error');
     }
+
 });
 
 
